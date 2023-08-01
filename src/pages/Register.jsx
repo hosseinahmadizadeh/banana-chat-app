943d3002b4898205f6f609dc175a317a339913c2
hosseinahmadizadeh <hosseinahmadizadeh1377@gmail.com>
2023-08-01 13:13:45 +0330

    development of Register component

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import Add from "../images/avatar/person.png";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className="form-container">
+      <div className="form-wrapper">
+        <span className="logo">Squash Chat</span>
+        <span className="title">Register</span>
+        <form>
+          <input type="text" placeholder="Name" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <input type="file" id="file" style={{ display: "none" }} />
+          <label htmlFor="file">
+            <img src={Add} alt="avatar" />
+            <span>Add an avatar</span>
+          </label>
+          <button>Sign Up</button>
+        </form>
+        <p>Do you have an account?! Login</p>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
